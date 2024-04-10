513fdc62625ff3500e495fcba2955f3738aac729
Keith Wagner <244694+kpwags@users.noreply.github.com>
2024-04-10 17:40:53 -0400

    Adjusted Generator to Use New Notion DB

--- Services/NotionService.cs
+++ Services/NotionService.cs
@@ -58,7 +58,7 @@ public class NotionService
         {
             var title = page.Properties.FirstOrDefault(p => p.Key.ToLower() == "title");
             var author = page.Properties.FirstOrDefault(p => p.Key.ToLower() == "author");
-            var url = page.Properties.FirstOrDefault(p => p.Key.ToLower() == "url");
+            var url = page.Properties.FirstOrDefault(p => p.Key.ToLower() == "link");
             var category = page.Properties.FirstOrDefault(p => p.Key.ToLower() == "category");
 
             var titleValue = title.Value as TitlePropertyValue;
@@ -68,9 +68,9 @@ public class NotionService
 
             articles.Add(new Article
             {
-                Title = titleValue?.Title?.FirstOrDefault()?.PlainText ?? "",
-                Author = authorValue?.RichText?.FirstOrDefault()?.PlainText ?? "",
-                Url = urlValue?.Url ?? "",
+                Title = titleValue?.Title?.FirstOrDefault()?.PlainText.Trim() ?? "",
+                Author = authorValue?.RichText?.FirstOrDefault()?.PlainText.Trim() ?? "",
+                Url = urlValue?.Url.Trim() ?? "",
                 Category = GetCategoryFromNotionCategory(categoryValue?.Select.Name.ToLower() ?? ""),
             });
         }
